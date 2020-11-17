3be412095915f18c97ccacc65d151b5066051712
Eray Bakır <info@eraybakir.com>
2020-11-17 18:17:03 +0300

    ViewComponent Added

---
+++ WebFundamentals/ViewComponents/Urunler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFundamentals.ViewComponents
+{
+    public class Urunler:ViewComponent
+    {
+        public IViewComponentResult Invoke() //Asenkron bir yapı olduğu için Invoke metodunun çağırılması gerekiyor---> Components/Component-Name/Default.cshtml
+        {
+            var list = new ArrayList();
+            list.Add("1.eleman");
+            list.Add("2.eleman");
+
+            return View(list); //Componente Data gönderdik
+        }
+    }
+}
